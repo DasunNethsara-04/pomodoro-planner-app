6a83eaec3afc1152edc02543bf422109b9679448
Dasun <techsaralk.pro@gmail.com>
2024-12-17 23:10:44 +0530

    changed the code to OOP

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import customtkinter as ctk
-import requests
+from util import *
 
-def getResponse()->dict[str, str]|requests.RequestException:
-    try:
-        response: requests.Response = requests.get("https://api-pomodoroplanner.vercel.app/")
-        return response.json()
-    except requests.RequestException as e:
-        return e
+util: Util = Util()
 
-root = ctk.CTk()
-root.geometry("800x400")
-root.title("Pomodoro Planner")
+class Application(ctk.CTk):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.geometry("800x400")
+        self.title("Pomodoro Planner")
+        ctk.CTkLabel(self, text=f"{util.getResponse('api/author')['message']}", font=("Arial", 25)).pack()
 
-ctk.CTkLabel(root, text=f"{getResponse()['message']}", font=("Arial", 25)).pack()
 
-root.mainloop()
+if __name__ == "__main__":
+    app:Application = Application()
+    app.mainloop()
