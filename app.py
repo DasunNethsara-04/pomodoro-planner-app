2ca87d78ca99ab9ca44e0abdc83e74f949c926d7
Dasun <techsaralk.pro@gmail.com>
2024-12-17 23:21:56 +0530

    UI changed

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from util import *
 util: Util = Util()
 
 class Application(ctk.CTk):
-    def __init__(self, master=None):
+    def __init__(self, master=None) -> None:
         super().__init__(master)
         self.geometry("800x400")
-        self.title("Pomodoro Planner")
-        ctk.CTkLabel(self, text=f"{util.getResponse('api/author')['message']}", font=("Arial", 25)).pack()
+        self.title(util.getResponse("api/info")["app_name"])
+
+        ctk.CTkLabel(self, text=f"{util.getResponse('api/info')['app_name']}", font=("Calibri", 25)).pack()
 
 
 if __name__ == "__main__":
